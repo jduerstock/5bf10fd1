460d9d0f94f4c6b8dc25b7e3f5f5ee7be43fe86d
jason <jason@em64t.cluephone.com>
2009-11-03 08:25:52 -0500

    Tue Nov  3 08:25:52 EST 2009

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 m68k-unknown-linux-as --statistics tmp9 -o tmp9.o
 m68k-unknown-linux-ld --stats --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o bork tmp9.o || exit
 cmp ROM-077D.DAT bork || exit
-echo oink
+#echo oink
 csplit -s -n 3 -f 077d. tmp9 "/SPLIT HERE/" "{*}"
 git diff
 git add 077d.???
