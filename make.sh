9200f86927271bdc3268dcc1db8e8de88fd90966
jason <jason@em64t.cluephone.com>
2009-11-03 00:19:57 -0500

    Tue Nov  3 00:19:57 EST 2009

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 m68k-unknown-linux-as --statistics tmp9 -o tmp9.o
-m68k-unknown-linux-ld --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o bork tmp9.o || exit
+m68k-unknown-linux-ld --stats --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o bork tmp9.o || exit
 cmp ROM-077D.DAT bork || exit
 echo oink
 csplit -s -n 3 -f 077d. tmp9 "/SPLIT HERE/" "{*}"
