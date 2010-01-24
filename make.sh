1db6a2801239204f6cfb03fb4602a936e42d78d2
U-jason-PC\jason <jason@jason-PC.(none)>
2010-01-24 10:31:43 -0500

    Sun Jan 24 10:31:43 EST 2010

--- make.sh
+++ make.sh
@@ -3,8 +3,8 @@ m68k-unknown-linux-as --statistics tmp9 -o tmp9.o || exit
 m68k-unknown-linux-ld --stats --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o bork tmp9.o || exit
 cmp ROM-077D.DAT bork || exit
 #echo oink
-csplit -s -n 3 -f 077d. tmp9 "/SPLIT HERE/" "{*}"
+#csplit -s -n 3 -f 077d. tmp9 "/SPLIT HERE/" "{*}"
 git diff
 git add 077d.???
 git commit -m "$(date)"
-rm 077d.[0-9]??
+#rm 077d.[0-9]??
