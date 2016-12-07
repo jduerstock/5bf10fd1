2e28b86cde2ca14e32c58ddeed53872dc233f57f
Jason Duerstock <jason.duerstock@gmail.com>
2016-12-06 20:17:49 -0500

    Tue Dec  6 20:17:49 EST 2016

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-m68k-unknown-linux-as -g --statistics -o 5bf10fd1.o 5bf10fd1.a || exit
-m68k-unknown-linux-ld --stats --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o 5bf10fd1.bin 5bf10fd1.o || exit
+m68k-linux-gnu-as -g --statistics -o 5bf10fd1.o 5bf10fd1.a || exit
+m68k-linux-gnu-ld --stats --oformat=binary -Ttext=0x40800000 -e 0x40800000 -o 5bf10fd1.bin 5bf10fd1.o || exit
 #cmp ROM-077D.DAT 5bf10fd1.bin || exit
 sha1sum -c 5bf10fd1.sha1 || exit
 #echo oink
